8327750681a90a909cb807078685f19a2d145b11
架构之路在何方 <li7hai26@outlook.com>
2022-01-16 17:49:59 +0800

    网关微服务平台初始化-调整推送接口管理平台配置,并推送正式数据进行展示

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 ENVIRONMENT="$1"
+
+check_env() {
+  if [ -z $ENVIRONMENT ]; then
+    echo "Usage: $0 {dev|sit|uat|pp|prod}"
+    exit 1;
+  fi
+
+  CMD_ARRAY=(java node docker docker-compose)
+  for i in ${CMD_ARRAY[@]} ; do
+    type $i
+    if [ "$?" -ne 0 ]; then
+      echo "$i command not found."
+      exit 1;
+    fi
+  done
+}
+
+check_env
+
 SOURCE="$0"
 while [ -h "$SOURCE"  ]; do
     DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
@@ -17,5 +36,11 @@ yarn
 yarn build:docker
 
 cd $DIR
+echo '开始打包后端包'
 mvn clean package -P${ENVIRONMENT} -Dmaven.test.skip=true
+echo '结束打包后端包'
+
+cd $DIR
+echo '开始打包镜像'
 docker-compose -f docker-compose-build.yaml build
+echo '结束打包镜像'
