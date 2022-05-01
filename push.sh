4ea61d974ab58374ab94c05ff67678a66ecca8c8
架构之路在何方 <li7hai26@outlook.com>
2022-05-02 01:08:38 +0800

    网关微服务平台初始化-推送镜像增加sentinel

--- push.sh
+++ push.sh
@@ -5,4 +5,5 @@ docker push dlhf/gateway-admin-bootstrap:$image_version
 docker push dlhf/gateway-admin-ui:$image_version
 docker push dlhf/gateway-bootstrap:$image_version
 docker push dlhf/monitor:$image_version
+docker push dlhf/sentinel-dashboard:$image_version
 echo '结束推送镜像至dockerhub仓库'
